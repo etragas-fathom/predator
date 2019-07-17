2566ef3148f0dd686ee84ca7f2fa39005c6f945e
etragas-fathom <elias@fathomhealth.co>
2019-07-16 22:15:01 -0400

    init commit

---
+++ src/main.rs
@@ -0,0 +1,45 @@
+extern crate reqwest;
+use std::collections::HashMap;
+use serde_json::{Value};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize)]
+struct CatList {
+    body: Vec<Cat>
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Cat{
+    actQuickly: bool,
+    age: String,
+    bondedTo: u32,
+    breedId: u32,
+    breedName: String,
+    city: String,
+    clanId: u32, // What does this mean?
+    description: String,
+    image: String,
+    name: String,
+    petId: u32,
+    sex: String,
+    specialNeeds: bool,
+    state: String,
+    status: String,
+}
+
+
+fn main() -> Result<(), Box<std::error::Error>> {
+    let mut resp = reqwest::get("https://ra-api.adoptapet.com/v1/shelters/75215/pets/?start=1&limit=100")?.text()?;
+    let cats: Value = serde_json::from_str(&resp)?;
+    println!("{:#?}", cats);
+    let cats: CatList = serde_json::from_str(&resp)?;
+    // let cats: Vec<CatList> = &resp["body"];
+    for obj in cats.body {
+        println!("Noot noot {:#?}", obj)
+    }
+    // println!("{:#?}", cats);
+    // for val in cats {
+        // println!("{:#?}", val);
+    // }
+    Ok(()) 
+}
