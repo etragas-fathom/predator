6cec0b9e080368742eac1df219f4a23c2089cea1
etragas-fathom <elias@fathomhealth.co>
2019-07-16 22:40:27 -0400

    format file

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
 extern crate reqwest;
-use std::collections::HashMap;
-use serde_json::{Value};
 use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::collections::HashMap;
 
 #[derive(Serialize, Deserialize)]
 struct CatList {
-    body: Vec<Cat>
+    body: Vec<Cat>,
 }
 
 #[derive(Serialize, Deserialize, Debug)]
-struct Cat{
+struct Cat {
     actQuickly: bool,
     age: String,
     bondedTo: u32,
@@ -27,9 +27,10 @@ struct Cat{
     status: String,
 }
 
-
 fn main() -> Result<(), Box<std::error::Error>> {
-    let mut resp = reqwest::get("https://ra-api.adoptapet.com/v1/shelters/75215/pets/?start=1&limit=100")?.text()?;
+    let mut resp =
+        reqwest::get("https://ra-api.adoptapet.com/v1/shelters/75215/pets/?start=1&limit=100")?
+            .text()?;
     let cats: Value = serde_json::from_str(&resp)?;
     println!("{:#?}", cats);
     let cats: CatList = serde_json::from_str(&resp)?;
@@ -39,7 +40,7 @@ fn main() -> Result<(), Box<std::error::Error>> {
     }
     // println!("{:#?}", cats);
     // for val in cats {
-        // println!("{:#?}", val);
+    // println!("{:#?}", val);
     // }
-    Ok(()) 
+    Ok(())
 }
