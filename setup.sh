ce8a5b3e14bdd297c83ede613808a2ba62d4f639
etragas-fathom <elias@fathomhealth.co>
2019-07-18 21:18:51 -0400

    readme

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 curl https://sh.rustup.rs -sSf | sh
 source $HOME/.cargo/env
-rustup component add rustfmt --toolchain stable-x86_64-apple-darwin
+rustup component add rustfmt --toolchain stable-x86_64-apple-darwin # This might need to be edited to accommodate different OSes
 cargo build
 cargo run
